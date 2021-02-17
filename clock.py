60ce0e0149f6c21242b4029b6e4d99f735cae2be
Adithya Balaji <adithyabsk@gmail.com>
2021-02-17 02:41:52 -0500

    Fix bugs.

--- clock.py
+++ clock.py
@@ -6,12 +6,12 @@ scheduler = BlockingScheduler()
 scheduler.add_job(lambda: print("This job is run every minute"), "cron", minute=1)
 
 
-@sched.scheduled_job('interval', seconds=30)
+@scheduler.scheduled_job('interval', seconds=30)
 def timed_job():
     print('This job is run every 30 seconds.')
 
 
-@sched.scheduled_job('cron', minute=2)
+@scheduler.scheduled_job('cron', minute=2)
 def timed_job():
     print('This job is run every 2 minutes.')
 
